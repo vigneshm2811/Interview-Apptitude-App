0cac515f05b808970f8445567a1a69f8fb74df42
Vignesh M. <CIPL1468@colanonline.local>
2023-12-26 19:28:40 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,50 @@
+
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+
+let stdUsername = document.getElementById("stdUsername");
+let stdPassword = document.getElementById("stdPassword");
+let adminUsername = document.getElementById("adminUsername");
+let adminPassword = document.getElementById("adminPassword");
+
+const userLoginBtn = document.getElementById("userLoginBtn");
+const adminLoginBtn = document.getElementById("adminLoginBtn");
+
+
+let adminAuth =document.getElementById("admin-login-p")
+ 
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+ 
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
+
+adminLoginBtn.addEventListener("click",()=>{
+    let username = adminUsername.value;
+    let password = adminPassword.value;
+    if(username === "admin" && password ==="admin"){
+        window.location.href = "./main/admin.html";
+    }
+    else{
+        adminAuth.innerHTML ="Enter valid Login"
+    }
+    adminUsername.value=""
+    adminPassword.value=""
+})
+
+
+userLoginBtn.addEventListener("click",()=>{
+    let username = stdUsername.value;
+   
+    console.log(username)
+
+    stdUsername.value=""
+    stdPassword.value=""
+  
+})
